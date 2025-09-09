395521689a124c932e0e532f8d0741da5727924e
aman <spercellcoc5@gmail.com>
2025-09-09 17:36:33 +0530

    commit from GitDemo guy

--- src/DateClassDemo.java
+++ src/DateClassDemo.java
@@ -4,17 +4,18 @@ import java.util.Date;
 public class DateClassDemo {
 
 	public static void main(String[] args) {
-		// TODO 
-		
+
 		Date d=new Date();
 		
 		System.out.println(d.toString());
 		
 		SimpleDateFormat sdf=new SimpleDateFormat("M/d/yyyy");
+		SimpleDateFormat sd1=new SimpleDateFormat("yyyy");
 		SimpleDateFormat sd=new SimpleDateFormat("M/d/yyyy hh:mm:ss");
 		
 		System.out.println(sdf.format(d));
 		System.out.println(sd.format(d));
+		System.out.println(sd1.format(d));
 
 	}
 
